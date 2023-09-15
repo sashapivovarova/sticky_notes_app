b8dadbec096791ec5b5b930c9bfe162e8788cf2a
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-09-15 15:24:00 +0900

    option button を作成

---
+++ lib/data/button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class IconButtonAnchorExample extends StatelessWidget {
+  const IconButtonAnchorExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MenuAnchor(
+      builder: (context, controller, child) {
+        return IconButton(
+          onPressed: () {
+            if (controller.isOpen) {
+              controller.close();
+            } else {
+              controller.open();
+            }
+          },
+          icon: const Icon(Icons.more_vert),
+        );
+      },
+      menuChildren: [
+        MenuItemButton(
+          child: const Text('Edit'),
+          onPressed: () {},
+        ),
+        MenuItemButton(
+          child: const Text('Delete'),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
