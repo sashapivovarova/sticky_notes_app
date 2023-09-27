acbc044a0b75f360128991ff0572b333512d5673
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2023-09-27 19:35:26 +0900

    onborading を修正

--- lib/page/onboarding_page.dart
+++ lib/page/onboarding_page.dart
@@ -8,79 +8,130 @@ class OnBoardingPage extends StatelessWidget {
   static const routeName = '/';
   const OnBoardingPage({super.key});
 
-  Future<bool> haveSeenIntro() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('haveSeenIntro') ?? false;
-  }
+  // Future<bool> haveSeenIntro() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   return prefs.getBool('haveSeenIntro') ?? false;
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: haveSeenIntro(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const SplashScreen();
-        }
+    // return FutureBuilder(
+    //   future: haveSeenIntro(),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.connectionState == ConnectionState.waiting) {
+    //       return const SplashScreen();
+    //     }
 
-        if (snapshot.error != null) {
-          return const SplashScreen();
-        }
-        if (snapshot.data != null) {
-          return const NoteListPage();
-        } else {
-          return IntroductionScreen(
-            showSkipButton: true,
-            skip: const Text(
-              'Skip',
-            ),
-            onSkip: () {
-              Navigator.pushNamed(context, '/list');
-            },
-            done: const Text(
-              'Start',
-            ),
-            onDone: () {
-              Navigator.pushNamed(context, '/list');
-            },
-            next: const Text(
-              'Next',
-            ),
-            dotsDecorator: DotsDecorator(
-              activeSize: const Size(
-                30,
-                10,
-              ),
-              activeShape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(
-                  30,
-                ),
-              ),
-            ),
-            pages: [
-              PageViewModel(
-                title: 'How to add',
-                body: 'Touch the plus button!',
-                image: Image.asset(
-                  'assets/add.png',
-                ),
-                decoration: getPageDecoration(),
-              ),
-              PageViewModel(
-                title: 'How to color',
-                body: 'Touch the palette button!',
-                image: Image.asset('assets/color.png'),
-                decoration: getPageDecoration(),
-              ),
-              PageViewModel(
-                title: 'How to save',
-                body: 'Touch the save button!',
-                image: Image.asset('assets/save.png'),
-                decoration: getPageDecoration(),
-              ),
-            ],
-          );
-        }
+    //     if (snapshot.error != null) {
+    //       return const SplashScreen();
+    //     }
+    //     if (snapshot.data != null) {
+    //       return const NoteListPage();
+    //     } else {
+    //       return IntroductionScreen(
+    //         showSkipButton: true,
+    //         skip: const Text(
+    //           'Skip',
+    //         ),
+    //         onSkip: () {
+    //           Navigator.pushNamed(context, '/list');
+    //         },
+    //         done: const Text(
+    //           'Start',
+    //         ),
+    //         onDone: () {
+    //           Navigator.pushNamed(context, '/list');
+    //         },
+    //         next: const Text(
+    //           'Next',
+    //         ),
+    //         dotsDecorator: DotsDecorator(
+    //           activeSize: const Size(
+    //             30,
+    //             10,
+    //           ),
+    //           activeShape: RoundedRectangleBorder(
+    //             borderRadius: BorderRadius.circular(
+    //               30,
+    //             ),
+    //           ),
+    //         ),
+    //         pages: [
+    //           PageViewModel(
+    //             title: 'How to add',
+    //             body: 'Touch the plus button!',
+    //             image: Image.asset(
+    //               'assets/add.png',
+    //             ),
+    //             decoration: getPageDecoration(),
+    //           ),
+    //           PageViewModel(
+    //             title: 'How to color',
+    //             body: 'Touch the palette button!',
+    //             image: Image.asset('assets/color.png'),
+    //             decoration: getPageDecoration(),
+    //           ),
+    //           PageViewModel(
+    //             title: 'How to save',
+    //             body: 'Touch the save button!',
+    //             image: Image.asset('assets/save.png'),
+    //             decoration: getPageDecoration(),
+    //           ),
+    //         ],
+    //       );
+    //     }
+    //   },
+    // );
+    return IntroductionScreen(
+      showSkipButton: true,
+      skip: const Text(
+        'Skip',
+      ),
+      onSkip: () {
+        Navigator.pushNamed(context, '/list');
       },
+      done: const Text(
+        'Start',
+      ),
+      onDone: () {
+        Navigator.pushNamed(context, '/list');
+      },
+      next: const Text(
+        'Next',
+      ),
+      dotsDecorator: DotsDecorator(
+        activeSize: const Size(
+          30,
+          10,
+        ),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            30,
+          ),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: 'How to add',
+          body: 'Touch the plus button!',
+          image: Image.asset(
+            'assets/add.png',
+          ),
+          decoration: getPageDecoration(),
+        ),
+        PageViewModel(
+          title: 'How to color',
+          body: 'Touch the palette button!',
+          image: Image.asset('assets/color.png'),
+          decoration: getPageDecoration(),
+        ),
+        PageViewModel(
+          title: 'How to save',
+          body: 'Touch the save button!',
+          image: Image.asset('assets/save.png'),
+          decoration: getPageDecoration(),
+        ),
+      ],
     );
   }
 }
